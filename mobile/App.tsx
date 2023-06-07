1a963c2e11250314016f8c25f1e7a9ab34b033d0
MWasile <maaciej.wasilewski@gmail.com>
2023-06-08 01:34:39 +0200

    websocket, sr dont work :(

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,8 @@
 import { NativeBaseProvider } from 'native-base';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Dupas from './components/dupa';
 import Home from './components/Home/Home';
+import RecordAudio from './components/RecordAudio/RecordAudio';
 
 
 const Stack = createStackNavigator();
@@ -13,7 +13,7 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Home1" component={Dupas} />
+                <Stack.Screen name="Home1" component={RecordAudio} />
             </Stack.Navigator>
         </NavigationContainer>
     </NativeBaseProvider>
--- mobile/components/RecordAudio/RecordAudio.tsx
+++ mobile/components/RecordAudio/RecordAudio.tsx
@@ -1,3 +1,72 @@
-function RecordAudio() {
-    return null;
-}
+import { Text, Button, Box } from "native-base";
+import { useEffect } from "react";
+import LiveAudioStream from 'react-native-live-audio-stream';
+import { encode } from 'base-64';
+
+const options = {
+    sampleRate: 32000, 
+    channels: 1, 
+    bitsPerSample: 16, 
+    audioSource: 6,
+    bufferSize: 4096 
+  };
+
+
+function RecordAudio({navigation}) {
+
+
+    useEffect(() => {
+    LiveAudioStream.start();
+      const socket = new WebSocket('ws://127.0.0.1:8000/ws/audio-analysis/');
+  
+      const options = {
+        sampleRate: 44100,  // default is 44100 but 32000 is adequate for accurate voice recognition
+        channels: 2,        // 1 or 2, default 1
+        bitsPerSample: 16,  // 8 or 16, default 16
+        // audioSource: 6,     // android only (see below)
+        bufferSize: 4096    // default is 2048
+      };
+
+      socket.onopen = () => {
+        
+        LiveAudioStream.init(options);
+        LiveAudioStream.on('data', (chunk) => {
+                socket.send(chunk);
+        });
+        console.log('Połączono z WebSocket');
+        
+      };
+  
+      socket.onmessage = (e) => {
+        console.log('Otrzymano wiadomość: ', e.data);
+      };
+  
+      socket.onclose = (e) => {
+        console.log('Połączenie WebSocket zamknięte: ', e.code, e.reason);
+      };
+  
+      socket.onerror = (e) => {
+        console.log('Błąd połączenia WebSocket: ', e.message);
+      };
+  
+      return () => {
+        if (socket) {
+          socket.close();
+        }
+      };
+    }, []);
+  
+    return (
+      <Box safeArea={8}>
+        <Button
+            onPress={() => {
+                navigation.navigate('Home');
+            }}
+        >Wróć</Button>
+      </Box>  
+    );
+
+  };
+
+
+  export default RecordAudio;
